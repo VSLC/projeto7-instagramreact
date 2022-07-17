916b82c9ad20f9225bfab0fe295e7a473a0f1256
Voller Silas <voller.silas@gmail.com>
2022-07-17 16:28:45 -0300

    Adicionando mensagem nos posts

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,3 +1,4 @@
+import React from "react"
 function Post(props) {
     return (
         <div class="posts">
@@ -28,7 +29,7 @@ function Post(props) {
 
             <div class="coluna-de-posts-curtiu flex">
                 <img src={props.img3} alt="responde aí" />
-                <p>{props.message}</p>
+                <p>Curtido por <strong>{props.usuario}</strong> e <strong>outras {props.numeroPessoas} pessoas</strong></p>
             </div>
 
         </div>
@@ -37,16 +38,18 @@ function Post(props) {
 
 export default function Feed() {
     const arrayPosts = [
-        { img1: "/img/9gag1.png", titulopersona: "meow", img2: "/img/gato-telefone.png", img3: "/img/respondeai1.png", message: "Curtido por Responde e outras 100.000 pessoas" },
-        { img1: "/img/9gag1.png", titulopersona: "Voller", img2: "/img/gato-telefone.png", img3: "/img/respondeai1.png", message: "Curtido por voller e outras 100.000 pessoas" },
-        { img1: "/img/9gag1.png", titulopersona: "Gabriel", img2: "/img/gato-telefone.png", img3: "/img/respondeai1.png", message: "Curtido por Castro e outras 100.000 pessoas" },
-        { img1: "/img/9gag1.png", titulopersona: "Antônio", img2: "/img/gato-telefone.png", img3: "/img/respondeai1.png", message: "Curtido por galo e outras 100.000 pessoas" }
+        { img1: "/img/9gag1.png", titulopersona: "meow", img2: "/img/gato-telefone.png", img3: "/img/respondeai1.png", usuario: "Respondeai" , numeroPessoas:5000 },
+        { img1: "/img/9gag1.png", titulopersona: "Voller", img2: "/img/gato-telefone.png", img3: "/img/respondeai1.png", usuario: "voller", numeroPessoas:100000 },
+        { img1: "/img/9gag1.png", titulopersona: "Gabriel", img2: "/img/gato-telefone.png", img3: "/img/respondeai1.png", usuario: "Castro", numeroPessoas:20000 },
+        { img1: "/img/9gag1.png", titulopersona: "Antônio", img2: "/img/gato-telefone.png", img3: "/img/respondeai1.png", usuario: "galo", numeroPessoas:30000}
     ];
+
+
 
     return (
         <div class="conteudo-principal">
             <div class="coluna-de-posts">
-                {arrayPosts.map((e) => <Post img1={e.img1} titulopersona={e.titulopersona} img2={e.img2} img3={e.img3} message={e.message} />)}
+                {arrayPosts.map((e) => <Post img1={e.img1} titulopersona={e.titulopersona} img2={e.img2} img3={e.img3} usuario={e.usuario} numeroPessoas={e.numeroPessoas} />)}
             </div>
         </div>
     );
